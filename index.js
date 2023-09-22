037a1bf3d4e1a10c358c19bd283b8f7461c0a3fb
Jorgecdl96 <100006802+Jorgecdl96@users.noreply.github.com>
2023-09-21 20:36:06 -0700

    connecting MongoDB database

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/phonebook')
 
 const app = express()
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
 let persons = [
     {
-        id: 1,
         name: "Arto Hellas",
         number: "040-123456"
     },
     {
-        id: 2,
         name: "Ada Lovelace",
         number: "039-123456"
     },
     {
-        id: 3,
         name: "Dan Abramov",
         number: "041-123456"
     },
     {
-        id: 4,
         name: "Mary Poppendick",
         number: "042-123456"
     },
@@ -42,75 +40,90 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.get('/info', (req, res)=> {
 
     const date = new Date()
-    
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
+
+    Phonebook.find({}).then(result => {
+
+        const info = `<p>Phonebook has info for ${result.length} people</p>
     <div> ${date}</div>`
 
     res.send(info)
+    })
+    
+    
 })
 
 app.get('/api/persons', (req, res) => {
 
-    res.json(persons)
+    Phonebook.find({}).then(persons => {
+
+        res.status(200).json(persons)
+    }).catch(err =>
+        console.log(err)
+    )
+
+    
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+    const id = req.params.id
 
-    const person = persons.find((person) => person.id === id)
+    Phonebook.findById(id)
+    .then(person => {
+        if (person) {
+           return res.json(person)
+        }
 
-    if (person) {
-        res.json(person)
-        return
-    }
-
-    res.status(404).end()
+        res.status(404).end()
+    })  
     
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+    const id = req.params.id
 
-    const person = persons.find(person => person.id === id)
+    Phonebook.findByIdAndRemove(id)
+        .then(person => {
+            if (person) {
+                return res.status(204).end()
+                
+            } else {
+                res.status(404).json({message: 'id not found'}).end()
+            }
+        })
 
-    if (person) {
-        persons = persons.filter((person) => person.id !== id) 
-
-        res.status(204).end()
-        return
-    } else {
-        res.status(404).json({message: 'id not found'}).end()
-    }
-    
     
 })
 
 app.post('/api/persons', (req, res) => {
+
     const {name, number} = req.body
 
     if (!name || !number) {
-        return res.status(400).json({error: 'name or number are missing'})
+        res.status(400).json({error: 'name or number are missing'})
+        return
     }
 
-    const alreadyAdded = persons.find(person => person.name === name)
+    Phonebook.findOne({name: name}).then(result => {
 
-    if(alreadyAdded){
-        return res.status(400).json({error: "name must be unique"})
-    }
+        if (result) {
+            res.status(400).json({error: "name must be unique"})
+            return
+        }
 
-    const person = {
-        id: Math.floor(Math.random()*10000 - 1),
-        name,
-        number
-    }
+        const newPerson = {
+            name,
+            number
+        }
+    
+        Phonebook.create(newPerson).then(result => {
+            res.status(200).json(result)
+        })
+    })
 
-    persons = persons.concat(person)
+    
 
-    res.status(200).json(person)
+
 })
-
-
-
 
 
 app.listen(PORT, ()  => {
